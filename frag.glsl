9b838d898a56176bd219cc05cb125501968bf75a
Ben Miller <bmiller@method.com>
2017-09-01 15:22:27 -0400

    works

--- frag.glsl
+++ frag.glsl
@@ -2,6 +2,9 @@ varying vec2 vUv;
 uniform float iGlobalTime;
 uniform sampler2D iChannel0;
 uniform sampler2D iChannel1;
+uniform bool iEvenTick;
+uniform bool iHorizVert;
+uniform float iRes;
 
 vec3 rgb2hsv(vec3 c)
 {
@@ -14,50 +17,102 @@ vec3 rgb2hsv(vec3 c)
     return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
 }
 
-vec3 compare(vec3 color1, vec3 color2, vec3 color3, float y){
-  // color1 is top, color2 is pixel, color3 is bottom
+vec3 compareY(vec3 color1, vec3 color2, vec3 color3, float y){
 
   vec3 hsv1 = rgb2hsv(color1);
   vec3 hsv2 = rgb2hsv(color2);
   vec3 hsv3 = rgb2hsv(color3);
-  float c1 = hsv1.z;
-  float c2 = hsv2.z;
-  float c3 = hsv3.z;
+  float c1 = hsv1.x;
+  float c2 = hsv2.x;
+  float c3 = hsv3.x;
 
-  if (mod(y, 2.0)==0.0){
-    //if top is greater than pixel: return top
-    if(c1 > c2){
-      return color1;
-    }
-    // if bottom is less than pixel: return bottom
-    if(c3 < c2){
-      return color3;
+  if(iEvenTick){
+    if(y == iRes) return color2;
+    if (mod(y, 2.0)==0.0){
+      if(c3 > c2){
+        return color3;
+      }else{
+        return color2;
+      }
+    }else{
+      if(c1 < c2){
+        return color1;
+      }else{
+        return color2;
+      }
     }
   }else{
-    //if bottom is less than pixel: return bottom
-    if(c3 < c2){
-      return color3;
-    }
-    //if top is greater than pixel: return top
-    if(c1 > c2){
-      return color1;
+    if(y == 0.0) return color2;
+    if (mod(y, 2.0)==0.0){
+      if(c1 < c2){
+        return color1;
+      }else{
+        return color2;
+      }
+    }else{
+      if(c3 > c2){
+        return color3;
+      }else{
+        return color2;
+      }
     }
   }
-  return color2;
-
 }
+
+vec3 compareX(vec3 color1, vec3 color2, vec3 color3, float x){
+
+  vec3 hsv1 = rgb2hsv(color1);
+  vec3 hsv2 = rgb2hsv(color2);
+  vec3 hsv3 = rgb2hsv(color3);
+  float c1 = hsv1.y;
+  float c2 = hsv2.y;
+  float c3 = hsv3.y;
+
+  if(iEvenTick){
+    if(x == iRes) return color2;
+    if (mod(x, 2.0)==0.0){
+      if(c3 > c2){
+        return color3;
+      }else{
+        return color2;
+      }
+    }else{
+      if(c1 < c2){
+        return color1;
+      }else{
+        return color2;
+      }
+    }
+  }else{
+    if(x == 0.0) return color2;
+    if (mod(x, 2.0)==0.0){
+      if(c1 < c2){
+        return color1;
+      }else{
+        return color2;
+      }
+    }else{
+      if(c3 > c2){
+        return color3;
+      }else{
+        return color2;
+      }
+    }
+  }
+}
+
 
 vec3 sample(sampler2D texture, vec2 uv) {
   vec3 pixel = texture2D(texture, uv).rgb;
 
-  vec3 tl = texture2D(texture, vec2(-1,-1)+uv).rgb;
-  vec3 t = texture2D(texture, vec2(0,-1)+uv).rgb;
-  vec3 tr = texture2D(texture, vec2(1,-1)+uv).rgb;
-  vec3 l = texture2D(texture, vec2(-1,0)+uv).rgb;
-  vec3 r = texture2D(texture, vec2(1,0)+uv).rgb;
-  vec3 bl = texture2D(texture, vec2(-1,1)+uv).rgb;
-  vec3 b = texture2D(texture, vec2(0,1)+uv).rgb;
-  vec3 br = texture2D(texture, vec2(1,1)+uv).rgb;
+  // vec3 tl = texture2D(texture, vec2(-1.0/iRes,-1.0/iRes)+uv).rgb;
+  vec3 t = texture2D(texture, vec2(0,-1.0/iRes)+uv).rgb;
+  // vec3 tr = texture2D(texture, vec2(1.0/iRes,-1.0/iRes)+uv).rgb;
+  vec3 l = texture2D(texture, vec2(-1.0/iRes,0)+uv).rgb;
+  vec3 r = texture2D(texture, vec2(1.0/iRes,0)+uv).rgb;
+  // vec3 bl = texture2D(texture, vec2(-1.0/iRes,1.0/iRes)+uv).rgb;
+  vec3 b = texture2D(texture, vec2(0,1.0/iRes)+uv).rgb;
+  // vec3 br = texture2D(texture, vec2(1.0/iRes,1.0/iRes)+uv).rgb;
 
   // vec3 v1 = compare(pixel, t);
   // vec3 v2 = compare(pixel, b);
@@ -67,7 +122,12 @@ vec3 sample(sampler2D texture, vec2 uv) {
   // vec3 h = compare(h1, h2);
   // vec3 final = compare(h,v);
 
-  return compare(t, pixel, b, floor(gl_FragCoord.y));
+  if(iHorizVert){
+    return compareX(l, pixel, r, floor(gl_FragCoord.x));
+  }else{
+    return compareY(t, pixel, b, floor(gl_FragCoord.y));
+  }
+
 }
 
 void main() {
--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-const THREE = require('three')
-const glslify = require('glslify')
+const THREE = require('three');
+const glslify = require('glslify');
 
 const frustumSize = 512;
 const aspect = 1.0;
 
-const camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0, 2000 );
-const scene = new THREE.Scene()
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( 512, 512 );
-renderer.setClearColor( 0x000000, 1 );
+const camera = new THREE.OrthographicCamera(
+  frustumSize * aspect / -2,
+  frustumSize * aspect / 2,
+  frustumSize / 2,
+  frustumSize / -2,
+  0,
+  2000
+);
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(0, 0);
+renderer.setClearColor(0x000000, 1);
 
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
+renderer.domElement.id = 'canvas';
 
 //our basic full-screen application and render loop
-let time = 0, i = 0
+let time = 0,
+  i = 0;
 
 //load up a test image
-const imageTexture = new THREE.TextureLoader().load( "baboon.png", (texture)=>{
+const imageTexture = new THREE.TextureLoader().load('baboon.png', texture => {
+  texture.minFilter = THREE.NearestFilter;
+  texture.magFilter = THREE.NearestFilter;
+  mat.uniforms.iRes.value = texture.image.width;
+  renderer.setSize(texture.image.width, texture.image.width);
   render();
 });
 
-let bufferA = new THREE.WebGLRenderTarget(
-  512,
-  512, {
-    minFilter: THREE.NearestFilter,
-    magFilter: THREE.NearestFilter,
-    format: THREE.RGBAFormat,
-    type: THREE.FloatType
-  }
-);
+let bufferA = new THREE.WebGLRenderTarget(512, 512, {
+  minFilter: THREE.NearestFilter,
+  magFilter: THREE.NearestFilter,
+  format: THREE.RGBAFormat,
+  type: THREE.FloatType
+});
 
-let bufferB = new THREE.WebGLRenderTarget(
-  512,
-  512, {
-    minFilter: THREE.NearestFilter,
-    magFilter: THREE.NearestFilter,
-    format: THREE.RGBAFormat,
-    type: THREE.FloatType
-  }
-);
+let bufferB = new THREE.WebGLRenderTarget(512, 512, {
+  minFilter: THREE.NearestFilter,
+  magFilter: THREE.NearestFilter,
+  format: THREE.RGBAFormat,
+  type: THREE.FloatType
+});
 
 //here we create a custom shader with glslify
 //note USE_MAP is needed to get a 'uv' attribute
@@ -48,22 +55,26 @@ const mat = new THREE.ShaderMaterial({
   fragmentShader: glslify('./frag.glsl'),
   uniforms: {
     iChannel0: { type: 't', value: imageTexture },
-    iChannel1: {type:'t', value: bufferA.texture},
-    iGlobalTime: { type: 'f', value: 0 }
+    iChannel1: { type: 't', value: bufferA.texture },
+    iGlobalTime: { type: 'f', value: 0 },
+    iEvenTick: { type: 'b', value: true },
+    iHorizVert: { type: 'b', value: true },
+    iRes: { type: 'f', value: 1 }
   },
   defines: {
     USE_MAP: ''
   }
-})
+});
 
-//make a box, hidden until the texture has loaded
 const geo = new THREE.PlaneGeometry(512.0, 512.0);
-const plane = new THREE.Mesh(geo, mat)
-scene.add(plane)
+const plane = new THREE.Mesh(geo, mat);
+scene.add(plane);
 
-var render = function () {
-  time += i / 1000
-  mat.uniforms.iGlobalTime.value = time
+var render = function() {
+  time += i / 1000;
+  mat.uniforms.iGlobalTime.value = time;
+  mat.uniforms.iEvenTick.value = i % 2 == 0;
+  mat.uniforms.iHorizVert.value = Math.floor(i / 2) % 2 == 0;
 
   renderer.render(scene, camera, bufferB, true);
 
@@ -73,8 +84,8 @@ var render = function () {
   // plane.material.map = bufferB.texture;
   mat.uniforms.iChannel1.value = bufferA.texture;
 
-  renderer.render( scene, camera );
-  // requestAnimationFrame( render );
-  setTimeout(render, 500);
-  i++
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+  // setTimeout(render, 500);
+  i++;
 };
